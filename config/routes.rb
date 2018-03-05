b1e82f622c6df66a5408fc170e240f465bbef949
joysa <joselin.solis.3b@gmail.com>
2018-03-05 19:21:20 -0300

    avance proyecto

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  get 'orders/create'
+
+ resources :products, only: :index do
+ 	resources :orders, only: :create
+ end
+
+ resources :orders, only: :index
+
+ root to: 'products#index'
  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root to: "users#index"
-get 'users/sign_up', to: "users#new"
-post 'users', to: "users#create"
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+
+12.times do |i|
+	Product.create(
+		name: Faker::Commerce.product_name,
+		price: Faker::Commerce.price.to_i,
+		photo: "https://loremflickr.com/320/240?random=#{i + 1}"
+		)
+end
