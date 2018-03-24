b518fccb40a0d91c7aad324f96dfa895804d827c
joysa <joselin.solis.3b@gmail.com>
2018-03-24 11:17:09 -0300

    cambios a heroku

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,6 +3,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	def facebook
 		@user = User.from_omniauth(request.env["omniauth.auth"])
 
+		puts "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+		puts @user
+		puts "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+
+	     puts "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+		puts @user.persisted?
+		puts "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+
 		if @user.persisted?
 			sign_in_and_redirect @user, event: :authentication
 			set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
